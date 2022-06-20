8199ccf34e197ca6eb58cbc58418eccda31ed166
blank <blank.c.vid@gmail.com>
2022-06-20 16:42:07 +0200

    feat: add pagination

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function () {
+  var button = $(".button");
+
+  function switchToNext() {
+    var _this = $(this);
+
+    if (_this.hasClass("active")) {
+      return false;
+    } else {
+      $(".button.active").removeClass("active");
+      _this.addClass("active");
+    }
+  }
+
+  button.on("click", switchToNext);
+});
